a9edfcaf978724d21e6a7faf6fcb398ab1f01e60
Rodrigo Collazo <collazorodrigo24@gmail.com>
2024-10-04 16:27:59 -0300

    Ejercicios 10-19

--- consultassqlpractica.sql
+++ consultassqlpractica.sql
@@ -49,6 +49,60 @@ WHERE V.CIUDADV <> P.CIUDADP AND P.CIUDADP<>Y.CIUDADY AND V.CIUDADV <> Y.CIUDADY
 SELECT vpy.NroP, PARTE, COLOR, PESO, CIUDADP
 FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
 		 INNER JOIN parte P ON vpy.NroP=P.NroP
-WHERE CIUDADV='Londres'
+WHERE CIUDADV='Londres';
 
 -- 10) Obtener los números de las partes suministradas por un proveedor de Londres para un proyecto en Londres.
+SELECT NroP
+FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
+		 INNER JOIN proyecto Y ON vpy.NroY=Y.NroY
+WHERE CIUDADV='Londres' AND CIUDADY='Londres';
+
+-- 11) Obtener todos los pares de nombres de ciudad tales que un proveedor en la primera ciudad suministre a un proyecto en la segunda ciudad.
+SELECT CIUDADV, CIUDADY
+FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
+		 INNER JOIN proyecto Y ON vpy.NroY=Y.NroY;
+
+-- 12) Obtener los números de las partes suministradas para cualquier proyecto por un proveedor en la misma ciudad del proyecto.
+SELECT NroP
+FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
+		 INNER JOIN proyecto Y ON vpy.NroY=Y.NroY
+WHERE CIUDADY=CIUDADV;
+
+-- 13) Obtener los números de los proyectos que suministra por lo menos un proveedor que no está en la misma ciudad.
+SELECT vpy.NroY
+FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
+		 INNER JOIN proyecto Y ON vpy.NroY=Y.NroY
+WHERE CIUDADY<>CIUDADV;
+
+-- 14) Obtener todos los pares de números de parte tales que algún proveedor suministre ambas partes indicadas.
+
+-- 15) Obtener los nombres de los proyectos que suministra el proveedor V1.
+SELECT Y.PROYECTO
+FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
+		 INNER JOIN proyecto Y ON vpy.NroY=Y.NroY
+WHERE V.PROVEEDOR='V1';
+
+-- 16) Obtener los colores de las partes suministradas por el proveedor V1.
+SELECT P.COLOR
+FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
+		 INNER JOIN parte P ON vpy.NroP=P.NroP
+WHERE V.PROVEEDOR='V1';
+
+-- 17) Obtener los números de las partes suministradas a cualquier proyecto en Londres.
+SELECT vpy.NroP
+FROM vpy INNER JOIN proyecto Y ON vpy.NroY=Y.NroY
+WHERE Y.CIUDADY='Londres';
+
+-- 18) Obtener los números de proyectos que usan al menos una parte disponible del proveedor V1.
+SELECT vpy.NroY
+FROM vpy INNER JOIN proveedor V ON vpy.NroV=V.NroV
+WHERE V.PROVEEDOR='V1';
+
+-- 19) Obtener los números de proveedores que suministran al menos una de las partes sumistradas al menos por un proveedor que a su vez suministra por lo menos una parte roja.
+SELECT distinct NroV
+FROM vpy
+WHERE vpy.NroP IN (SELECT vpy.NroP
+				  FROM vpy 
+				  WHERE vpy.NroV IN	(SELECT vpy.NroV
+									 FROM vpy INNER JOIN parte P ON vpy.NroP=P.NroP
+									 WHERE P.COLOR='ROJO'));
